2547f3bb4a483e41c6c9e44a06b9cd5f6e606139
Dennis von Bargen <dennis@vonbargen.se>
2021-10-06 18:25:08 +0200

    Migrated creation logic from terraform to create

--- create.go
+++ create.go
@@ -2,16 +2,63 @@ package main
 
 import (
     "bufio"
+    "fmt"
     "os"
     "strings"
+    "text/template"
 )
+
+type Instance struct {
+    Name      string
+    Hostname  string
+    Token     string
+    PublicKey string
+    Region    string
+    Type      string
+}
+
+var tfTemplate = `module "{{.Name}}" {
+  source = "git@github.com:Dekamik/vpn-modules.git//vpn-server?ref=v0.1.1"
+
+  token = "{{.Token}}"
+  public_keys = [
+    "{{.PublicKey}}"
+  ]
+
+  name = "{{.Hostname}}-{{.Name}}"
+  region = "{{.Region}}"
+  type = "{{.Type}}"
+}
+`
+
+func createFile(instance Instance) error {
+    homeDir, _ := os.UserHomeDir()
+    mkDirErr := os.MkdirAll(homeDir + "/.autovpn", 0777)
+    check(mkDirErr)
+
+    tmpl, tmplErr := template.New("tfmodule").Parse(tfTemplate)
+    if tmplErr != nil { return tmplErr }
+
+    filePath := fmt.Sprintf("%s/.autovpn/%s.tf", homeDir, instance.Name)
+    fmt.Println(filePath)
+    file, fileErr := os.Create(filePath)
+    if fileErr != nil { return tmplErr }
+    writer := bufio.NewWriter(file)
+
+    execErr := tmpl.Execute(writer, instance)
+    if execErr != nil { return execErr }
+    flushErr := writer.Flush()
+    if flushErr != nil { return flushErr }
+
+    return nil
+}
 
 func create(token string) error {
     var instances []Instance
     hostName, _ := os.Hostname()
     homeDir, _ := os.UserHomeDir()
 
-    sshFile, openErr := os.Open(homeDir + "/.ssh/id_ed25519.pub")
+    sshFile, openErr := os.Open(homeDir + "/.ssh/id_rsa.pub")
     if openErr != nil { return openErr }
     sshReader := bufio.NewReader(sshFile)
     publicKey, readErr := sshReader.ReadString('\n')
